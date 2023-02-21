49e8a29ec528e9627ab33b7655bf7d0660a049d4
Elliot Lartigaud <92160866+MrPoulet9987@users.noreply.github.com>
2023-02-21 14:31:19 +0100

    2

--- src/JDR/Main.java
+++ src/JDR/Main.java
@@ -1,9 +1,8 @@
 package JDR;
 
 public class Main {
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		Carte carte = new Carte(20, 20, 5, 3);
 		carte.afficher();
 	}
-
 }
